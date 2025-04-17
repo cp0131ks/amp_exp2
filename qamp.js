08ecb9c4d340d28dea74de8eb45c99404cd957c9
cp0131ks <cp0131ks@ed.ritsumei.ac.jp>
2025-04-17 19:33:54 +0900

    試行数の変更
    1ブロック25試行の計50試行に変更しました。

--- qamp.js
+++ qamp.js
@@ -126,7 +126,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 				image : 'https://baranan.github.io/minno-tasks/images/ampImages'
 			}, 
 
-			trialsInBlock : [1, 1], //Number of trials in each block 
+			trialsInBlock : [25, 25], //Number of trials in each block 
 			trialsInExample : 5, //Change to 0 if you don't want an example block
 			
 			//Duration parameters.
